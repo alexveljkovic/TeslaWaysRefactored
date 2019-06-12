0b20b8856315abf9e96ea41d5118ebbb95f386f6
antic11d <dimitrije.misa@gmail.com>
2019-06-12 12:06:50 +0200

    minor refactor

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Environment, GoogleMap, GoogleMapOptions, GoogleMaps, GoogleMapsEvent, Marker} from '@ionic-native/google-maps/ngx';
 import {AlertsService} from '../services/alert-service/alerts.service';
 import {Platform} from '@ionic/angular';
@@ -10,10 +10,9 @@ import {MapService} from '../services/map-service/map.service';
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
 })
-export class MapPage implements OnInit {
+export class MapPage implements OnInit, OnDestroy {
     map: GoogleMap;
     currentPos: Coordinates;
-    // currentPos: any;
   constructor(
       private alertsService: AlertsService,
       private platform: Platform,
@@ -27,52 +26,7 @@ export class MapPage implements OnInit {
       await this.mapService.loadMap();
       await this.location.pointLocation();
   }
-    //
-    // async loadMap() {
-    //     // This code is necessary for browser
-    //     /*
-    //     Environment.setEnv({
-    //         API_KEY_FOR_BROWSER_RELEASE: 'AIzaSyAfw3u7ZmqYhC6EXOsPLizVoYy00T9wn6M',
-    //         API_KEY_FOR_BROWSER_DEBUG: 'AIzaSyAfw3u7ZmqYhC6EXOsPLizVoYy00T9wn6M',
-    //     });*/
-    //
-    //   Environment.setEnv({
-    //       API_KEY_FOR_BROWSER_RELEASE: 'AIzaSyAz1sU7SQbb-IUY6JlXr3xWDmrQobct5aM',
-    //       API_KEY_FOR_BROWSER_DEBUG: 'AIzaSyAz1sU7SQbb-IUY6JlXr3xWDmrQobct5aM',
-    //   });
-    //
-    //   console.log('Pre getposition');
-    //   this.currentPos = await this.location.getPositionCoords();
-    //
-    //   const mapOptions: GoogleMapOptions = {
-    //       camera: {
-    //           target: {
-    //               lat: this.currentPos.latitude,
-    //               lng: this.currentPos.longitude
-    //           },
-    //           zoom: 18,
-    //           tilt: 30
-    //       }
-    //   };
-    //
-    //   const mapCanvas = document.getElementById('map_canvas');
-    //   mapCanvas.style.height = window.innerHeight * 9 / 10 + 'px';
-    //   this.map = GoogleMaps.create(mapCanvas, mapOptions);
-    //
-    //   await this.map.one(GoogleMapsEvent.MAP_READY);
-    //
-    //   console.log('Map ready triggered');
-    //   const marker: Marker = this.map.addMarkerSync({
-    //       title: 'Initial location',
-    //       icon: 'blue',
-    //       animation: 'DROP',
-    //       position: {
-    //           lat: this.currentPos.latitude,
-    //           lng: this.currentPos.longitude
-    //       }
-    //   });
-    //   marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
-    //   });
-    // }
-
+  async ngOnDestroy() {
+      await this.location.unsubscribeWatcher();
+  }
 }
--- src/app/services/location-service/location.service.ts
+++ src/app/services/location-service/location.service.ts
@@ -4,42 +4,46 @@ import {AlertsService} from '../alert-service/alerts.service';
 import {CameraPosition, GoogleMap, LatLng} from '@ionic-native/google-maps/ngx';
 import {ILatLng} from '@ionic-native/google-maps/ngx';
 import {MapService} from '../map-service/map.service';
+import {Subscription} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class LocationService {
-  data: Geoposition;
-  // map: GoogleMap;
-  constructor(private geolocation: Geolocation,
-              private alertService: AlertsService,
-              private mapService: MapService) { }
+    data: Geoposition;
+    subscription: Subscription;
 
-  async getPositionCoords() {
-    try {
-      this.data = await this.geolocation.getCurrentPosition({enableHighAccuracy: true});
-      console.log(`[location-service] lat: ${this.data.coords.latitude} lon: ${this.data.coords.longitude}`);
-      return this.data.coords;
-    } catch (e) {
-      console.log(e);
-      await this.alertService.displayError('Error with geolocation', 'Error getting location');
-    }
-  }
+    constructor(private geolocation: Geolocation,
+                private alertService: AlertsService,
+                private mapService: MapService) { }
 
-  async pointLocation() {
-    try {
-    // TODO: timeout not working?
-      const watcher = this.geolocation.watchPosition({enableHighAccuracy: true, timeout: 100000, maximumAge: 5000});
-      watcher.subscribe((data: any) => {
-        const latlng: LatLng = new LatLng(data.coords.latitude, data.coords.longitude);
-        this.mapService.setTarget(latlng);
-      });
-    } catch (e) {
-      console.log(e);
-      await this.alertService.displayError('Error with map', 'Error while watching position');
+    async getPositionCoords() {
+        try {
+            this.data = await this.geolocation.getCurrentPosition({enableHighAccuracy: true});
+            console.log(`[location-service] lat: ${this.data.coords.latitude} lon: ${this.data.coords.longitude}`);
+            return this.data.coords;
+        } catch (e) {
+            console.log(e);
+            await this.alertService.displayError('Error with geolocation', 'Error getting location');
+        }
     }
 
+    async pointLocation() {
+        try {
+            // TODO: timeout not working?
+            const watcher = this.geolocation.watchPosition({enableHighAccuracy: true, timeout: 100000, maximumAge: 5000});
+            this.subscription = watcher.subscribe((data: any) => {
+                const latlng: LatLng = new LatLng(data.coords.latitude, data.coords.longitude);
+                this.mapService.setTarget(latlng);
+            });
+        } catch (e) {
+            console.log(e);
+            await this.alertService.displayError('Error with map', 'Error while watching position');
+        }
+    }
 
-  }
+    async unsubscribeWatcher() {
+        this.subscription.unsubscribe()
+    }
 }
 
